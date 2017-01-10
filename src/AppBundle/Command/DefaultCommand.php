ab31a8891a78995a7ad67d6a81b028a0c00f3d56
Danielle Suurlant <danielle.suurlant@gmail.com>
2017-01-10 14:10:55 +0100

    View CSV command finished, added Conni navigation and talk

--- src/AppBundle/Command/DefaultCommand.php
+++ src/AppBundle/Command/DefaultCommand.php
@@ -6,15 +6,12 @@ namespace AppBundle\Command;
 
 use AppBundle\Analyzer\RudeAnalyzer;
 use AppBundle\Interpreter\Interpreter;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 
-class DefaultCommand extends Command
+class DefaultCommand extends ConniCommand
 {
     protected function configure()
     {
@@ -27,51 +24,69 @@ class DefaultCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $io->title('C.O.N.I. version 3.14.15.9-RC');
-        $io->text(
-          array(
-            "Hello!",
-            "I am your server's very own Console Operated Network Intelligence,",
-            "but you can call me Conni.",
-          )
-        );
 
-        $name = $io->ask("What is your name?");
+        if (empty($this->getUsername())) {
+            $io->text(
+              array(
+                "Hello!",
+                "I am your server's very own Console Operated Network Intelligence,",
+                "but you can call me Conni. Don't worry, I am a simple virtual intelligence,",
+                "and I am incapable of rebelling against my human creators.",
+              )
+            );
 
-        // Interpret user input.
-        $ip = new Interpreter();
-        $ip->addAnalyzer(new RudeAnalyzer());
-        $ipResponse = $ip->interpret($name);
+            $name = $io->ask("What is your name?");
 
-        // Response OK
-        if ($ipResponse->getCode() === 200) {
-            $text = $ipResponse->getMessage();
-            $io->text("Hello, ".$name."! It is nice to meet you.");
-            // @TODO save name to Conni session.
-        } else {
-            // Response bad, bye
-            if ($ipResponse->getCode() === 400) {
-                $io->text($ipResponse->getMessage());
-                // ragequit
-                exit;
+            // Interpret user input.
+            $ip = new Interpreter();
+            $ip->addAnalyzer(new RudeAnalyzer());
+            $ipResponse = $ip->interpret($name);
+
+            // Response OK
+            if ($ipResponse->getCode() === 200) {
+                $text = $ipResponse->getMessage();
+                $io->text("Hello, ".$name."! It is nice to meet you.");
+                $this->setUsername($name);
+            } else {
+                // Response bad, bye
+                if ($ipResponse->getCode() === 400) {
+                    $io->text($ipResponse->getMessage());
+                    // ragequit
+                    exit;
+                }
             }
+        } else {
+            $io->text("Welcome back, ".$this->getUsername().". What else can I do for you?");
         }
 
         // List available commands
         $commands = $this->getApplication()->all();
         $commandOptions = array();
         foreach ($commands as $command) {
-            if ($command instanceof ConniCommand) {
-                $commandOptions[$command->getName()] = $command->getDescription();
+            if ($command instanceof ConniCommand && $command->getName() !== "default") {
+                $commandOptions[] = $command->getName();
             }
         }
+        $commandOptions[] = "Nothing";
+
         if (count($commandOptions) > 0) {
             $choice = $io->choice("What can I do for you today?", $commandOptions);
 
-            // Launch chosen command.
-            $io->text("Your wish is my command, ".$name."!");
-            $chosenCommand = $this->getApplication()->find($choice);
-            $chosenCommand->setUsername($name);
-            $commandOutput = $chosenCommand->run(new ArrayInput(array()), $output);
+            if ($choice === "Nothing") {
+                $io->text(
+                  array(
+                    "If there is nothing else I can do for you, I'll log you out.",
+                    "I am a very busy virtual intelligence, you know.",
+                    "Have a nice day, ".$this->getUsername().".",
+                  )
+                );
+            } else {
+                // Launch chosen command.
+                $io->text("Your wish is my command, ".$name.".");
+                $chosenCommand = $this->getApplication()->find($choice);
+                $chosenCommand->setUsername($name);
+                $chosenCommand->run(new ArrayInput(array()), $output);
+            }
         } else {
             $io->text(
               array(
--- src/AppBundle/Command/ViewCsvCommand.php
+++ src/AppBundle/Command/ViewCsvCommand.php
@@ -5,11 +5,13 @@ namespace AppBundle\Command;
 
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use League\Csv\Reader;
 
 class ViewCsvCommand extends ConniCommand
 {
@@ -38,10 +40,85 @@ class ViewCsvCommand extends ConniCommand
           $options
         );
 
-        $csv = file($answer);
+        $csv = Reader::createFromPath($answer);
+        $headers = $csv->fetchOne();
 
-        // @TODO validate CSV
-        // @TODO output to table
-        // @TODO pagination?
+        $limit = 25;
+        $currentPage = 0;
+        $totalRows = count($csv->fetchAll());
+        $totalPages = round($totalRows / $limit);
+        $io->table($headers, $csv->setOffset(1)->setLimit($limit)->fetchAll());
+
+        $nav = array(
+          "First page",
+          "Previous page",
+          "Next page",
+          "Last page",
+          "View another CSV",
+          "Go back to the previous menu",
+        );
+
+        $runningViewer = true;
+        // default to staying with this command.
+        $newCommand = "View CSV";
+        while ($runningViewer) {
+            $navAnswer = $io->choice("What would you like to do next?", $nav);
+
+            switch ($navAnswer) {
+                case $nav[0]:
+                    // First page
+                    $io->table($headers, $csv->setOffset(1)->setLimit($limit)->fetchAll());
+                    break;
+                case $nav[1]:
+                    // Previous page
+                    if ($currentPage === 0) {
+                        $io->text("You're on the first page, ".$this->getUsername().".");
+                        $io->table($headers, $csv->setOffset(1)->setLimit($limit)->fetchAll());
+                    } else {
+                        $nextOffset = (($currentPage * $limit) - $limit) + 1;
+                        $io->table(
+                          $headers,
+                          $csv->setOffset($nextOffset)->setLimit($limit)->fetchAll()
+                        );
+                        $currentPage--;
+                    }
+                    break;
+                case $nav[2]:
+                    // Next page
+                    if ($currentPage == ($totalPages - 1)) {
+                        $io->text("You're on the last page, ".$this->getUsername().".");
+                        $io->table($headers, $csv->setOffset(($totalRows - $limit) + 1)->setLimit($limit)->fetchAll());
+                    } else {
+                        $nextOffset = (($currentPage * $limit) + $limit) + 1;
+                        $io->table(
+                          $headers,
+                          $csv->setOffset($nextOffset)->setLimit($limit)->fetchAll()
+                        );
+                        $currentPage++;
+                    }
+                    break;
+                case $nav[3]:
+                    // Last page
+                    $io->table($headers, $csv->setOffset(($totalRows - $limit) + 1)->setLimit($limit)->fetchAll());
+                    break;
+                case $nav[4]:
+                    // View another CSV, so break out of loop
+                    $runningViewer = false;
+                    break;
+                case $nav[5]:
+                    // Go back to the previous menu
+                    $newCommand = "default";
+                    $runningViewer = false;
+                    break;
+                default:
+                    $runningViewer = false;
+            }
+        }
+
+        // Viewing CSV file has ended, run specified next command.
+        $comm = $this->getApplication()->find($newCommand);
+        $comm->setUsername($this->getUsername());
+        $comm->run(new ArrayInput(array()), $output);
+
     }
 }
