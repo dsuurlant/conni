594c252debcb54d7ad0698c016f3eda99443c05b
Danielle Suurlant <danielle.suurlant@gmail.com>
2017-01-13 16:04:28 +0100

    Testing of multiple names

--- tests/AppBundle/Command/DefaultCommandTest.php
+++ tests/AppBundle/Command/DefaultCommandTest.php
@@ -3,11 +3,9 @@
 
 namespace Tests\AppBundle\Command;
 
-use AppBundle\Command\ConniCommand;
 use AppBundle\Command\DefaultCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Tester\CommandTester;
 
 
@@ -25,26 +23,100 @@ class DefaultCommandTest extends \PHPUnit_Framework_TestCase
     /** @var CommandTester */
     protected $tester;
 
+    /** @var Application */
+    private $app;
+
+    protected function setUp()
+    {
+        $app = new Application();
+        $app->add(new DefaultCommand());
+        $this->command = $app->find('default');
+        $this->tester = new CommandTester($this->command);
+    }
+
     protected function tearDown()
     {
         $this->command = null;
         $this->tester = null;
     }
 
-    /**
-     * Test asking for user's name.
-     */
-    public function testAskForName()
+    public function testName()
     {
-        $app = new Application();
-        $app->add(new DefaultCommand());
-        $this->command = $app->find('default');
-        $this->tester = new CommandTester($this->command);
         $this->tester->setInputs(array('John', 'Nothing'));
-
         $this->tester->execute(array('command' => $this->command->getName()));
         $this->assertContains('Hello, John!', $this->tester->getDisplay(true));
     }
 
+    public function testWelcomeBack()
+    {
+        $this->tester->setInputs(array('John', 'Nothing'));
+        $this->tester->execute(array('command' => $this->command->getName()));
+        $this->assertContains('Hello, John!', $this->tester->getDisplay(true));
+
+        $this->tester->setInputs(array('Nothing'));
+        $this->tester->execute(array('command' => $this->command->getName()));
+        $this->assertContains('Welcome back, John.', $this->tester->getDisplay(true));
+    }
+
+    public function testNormalNames()
+    {
+        $names = array(
+          "Bruce",
+          "Tony",
+          "Steve",
+          "Clint",
+          "Natascha",
+          "James",
+          "Nick",
+        );
+
+        foreach ($names as $name) {
+            $this->setUp();
+            $this->tester->setInputs(array($name, 'Nothing'));
+            $this->tester->execute(array('command' => $this->command->getName()));
+            $this->assertContains("Hello, ".$name."!", $this->tester->getDisplay(true));
+            $this->tearDown();
+        }
+    }
+
+    public function testSpecialNames()
+    {
+        $names = array(
+          "Daniëlle",
+          "André",
+          "Wojçek",
+          "Björk",
+          "María",
+          "Shōji",
+        );
+
+        foreach ($names as $name) {
+            $this->setUp();
+            $this->tester->setInputs(array($name, 'Nothing'));
+            $this->tester->execute(array('command' => $this->command->getName()));
+            $this->assertContains("Hello, ".$name."!", $this->tester->getDisplay(true));
+            $this->tearDown();
+        }
+    }
+
+    public function testRudeNames()
+    {
+        $names = array(
+          "Ass",
+          "Butt",
+          "Wanker",
+          "Fuck",
+          "Shit",
+          "Bastard",
+        );
+
+        foreach ($names as $name) {
+            $this->setUp();
+            $this->tester->setInputs(array($name));
+            $this->tester->execute(array('command' => $this->command->getName()));
+            $this->assertContains('rude', $this->tester->getDisplay(true));
+            $this->tearDown();
+        }
+    }
 
 }
